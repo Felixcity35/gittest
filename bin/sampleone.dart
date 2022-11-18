a8e6f561d384f7f95011c34eb39dd8a9198db7ee
felixcity <Felixarchibong@gmail.com>
2022-11-18 13:55:50 +0100

    second example

--- bin/sampleone.dart
+++ bin/sampleone.dart
@@ -1,12 +1,26 @@
 import "dart:io";
 
 void main(List<String> arguments) {
-  print("Enter your name");
-  String? name = stdin.readLineSync();
+  print("Problem 2 Solution");
 
-  print("Enter your Age");
-  int? age = int.parse(stdin.readLineSync()!);
+  List<String> stdName = [];
 
-  print("my name is $name");
-  print("i am $age years old.");
+  print("Enter the total number of Student in the Class");
+  int? classMax = int.parse(stdin.readLineSync()!);
+
+  for (var i = 0; i <= classMax; i++) {
+    print("Enter the name of $i student ");
+    String? name = stdin.readLineSync();
+    stdName.add(name!);
+  }
+
+  for (var name in stdName) {
+    print("======= printing out the student name ============");
+
+    print("Name of student  is  : $name ");
+  }
+  // print("Enter your Age");
+  // int? age = int.parse(stdin.readLineSync()!);
+  // print("my name is $name");
+  // print("i am $age years old.");
 }
